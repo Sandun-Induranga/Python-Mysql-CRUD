a60698742abc2c1941318ea72b8b17b1c2ac2384
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-08 22:41:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+import mysql.connector
 
 """
 * @author : Sandun Induranga
@@ -9,31 +9,19 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://sandu:1234@localhost[:3306/POS"
-app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-
-db = SQLAlchemy(app)
-
-
-class Customer(db.Model):
-    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    name = db.Column(db.String(100), nullable=False)
-    address = db.Column(db.String(255), nullable=False)
-    salary = db.Column(db.String(20), nullable=False)
-
-    def __init__(self, name, address, salary):
-        self.name = name
-        self.address = address
-        self.salary = salary
+cnx = mysql.connector.connect(user='sandu', password='1234', database='POS')
+cursor = cnx.cursor()
 
 
 @app.route('/')
 def index():
 
-    print(db.session.query(Customer))
-    return render_template('index.html')
+    query = 'SELECT * FROM Customer'
+    cursor.execute(query)
+    for customerId in cursor:
+        print(customerId)
+    # return render_template('index.html', query)
 
 
 if __name__ == '__main__':
     app.run()
-
