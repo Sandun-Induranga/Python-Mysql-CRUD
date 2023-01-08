8936648cc691f6fd49825c17bbceeb7c535d2aee
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-08 11:29:35 +0530

    Added Bootstrap

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def index():  # put application's code here
-    return render_template()
+def index():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
