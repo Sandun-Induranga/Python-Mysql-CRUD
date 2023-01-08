9ce7ea282f284304aa3200f2b1ffbd109a97b4d5
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-08 13:32:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 
+"""
+* @author : Sandun Induranga
+* @since : 0.1.0
+"""
+
 app = Flask(__name__)
 
 
@@ -10,10 +15,25 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(100), nullable=False)
+    address = db.Column(db.String(255), nullable=False)
+    salary = db.Column(db.String(20), nullable=False)
+
+    def __init__(self, name, address, salary):
+        self.name = name
+        self.address = address
+        self.salary = salary
+
+
 @app.route('/')
 def index():
+
+    print(db.session.query(Customer))
     return render_template('index.html')
 
 
 if __name__ == '__main__':
     app.run()
+
