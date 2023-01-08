7f847ef9b55152a30c40f494500d0ea6212e798e
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-08 12:22:53 +0530

    Content Updated

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+mysqlconnector://sandu:1234@localhost[:3306/POS"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+
+db = SQLAlchemy(app)
 
 
 @app.route('/')
