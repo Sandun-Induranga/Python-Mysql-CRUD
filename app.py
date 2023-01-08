0ae061451efc266c101c64f056912a327bda534b
Sandun-Induranga <sandutharu40@gmail.com>
2023-01-08 11:24:32 +0530

    Index.html Created

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def index():  # put application's code here
+    return render_template()
 
 
 if __name__ == '__main__':
