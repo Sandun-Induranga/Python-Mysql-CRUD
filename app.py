a4cad102c7228ac0a4abcf0cd4803c3d70863155
Sandun Induranga <88975401+Sandun-Induranga@users.noreply.github.com>
2023-01-10 11:59:03 +0530

    Content Updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import mysql.connector
 
 """
@@ -8,30 +8,26 @@ import mysql.connector
 
 app = Flask(__name__)
 
-
 cnx = mysql.connector.connect(user='sandu', password='1234', database='POS')
 cursor = cnx.cursor()
 
 
 @app.route('/')
 def index():
-
     query = 'SELECT * FROM Customer'
     cursor.execute(query)
-    for customerId in cursor:
-        print(customerId)
-    # return render_template('index.html', query)
-    
-    
+
+    return render_template('index.html', customers=cursor)
+
+
 @app.route('/savecustomer', methods=['get', 'post'])
 def savecustomer():
     id = request.form['cusId']
     name = request.form['cusName']
     address = request.form['address']
     salary = request.form['salary']
-    print(id)
-    return render_template('index.html', customers=cursor)
-    
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run()
